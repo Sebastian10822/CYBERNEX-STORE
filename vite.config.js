a78c421843792345f9633514c18d9bb8b8b23f20
Sebastián Briñez <johansebastianbrinezparra@gmail.coml>
2026-01-30 11:03:03 -0500

    Initial commit CYBERNEX STORE

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/TIENDA CYBERNEX/",
+  base: '/CYBERNEX-STORE/' // EXACTO el nombre de tu repo
 })
