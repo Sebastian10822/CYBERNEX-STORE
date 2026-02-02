bade803420e238f218d3717dea6e752430d1644d
Sebastián Briñez <johansebastianbrinezparra@gmail.coml>
2026-02-01 20:22:11 -0500

    Fix base path for github pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: "/CYBERNEX-STORE/",
   plugins: [react()],
-  base: '/CYBERNEX-STORE/' // EXACTO el nombre de tu repo
 })
